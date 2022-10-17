6070285bd96d5963a138b0287f965a8c5bd395f5
PR1CETAG <cprice@atl.yearup.og>
2022-10-17 13:41:13 -0400

    scripts

---
+++ Scripts/c.js
@@ -0,0 +1,12 @@
+let originalInput;
+let convertButton;
+let output;
+
+document.addEventListener("DOMContentLoaded",()=>{
+    originalInput = document.getElementById("orginalInput")
+    convertButton = document.getElementById("convertButton");
+    output = document.getElementById("output");
+}
+convertButton.addEventListener("click",()=>{
+
+}
